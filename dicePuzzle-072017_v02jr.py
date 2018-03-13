cdd80aac31b552611868f66675100593f40060c5
Justin Reusch <reuschj@gmail.com>
2018-03-13 14:20:54 -0500

    Add person class and updated class functionality

--- dicePuzzle-072017_v02jr.py
+++ dicePuzzle-072017_v02jr.py
@@ -10,32 +10,61 @@ import enum
 # Define Objects
 #_________________________________________________________________________________________
 
+class Person(object):
+    """represents a person who can posess one or more dice"""
+
+    def __init__(self, name, die = None):
+        self.name = name
+        if die == None:
+            self.dice = []
+        else:
+            self.dice = [die]
+            die.owner = self
+
+    def __str__(self):
+        diceStatement = ""
+        numDice = len(self.dice)
+        dieStr = "die" if numDice == 1 else "dice"
+        if numDice == 0:
+            diceStatement = "I don't have any dice yet."
+        else:
+            diceStatement += "I have %s %s with values:\n" % (str(numDice), dieStr)
+            for i in range(numDice):
+                diceStatement += "Die %s (%s sides): %s" % (str(i + 1), str(self.dice[i].numSides), buildListString(self.dice[i].sides))
+                if i < numDice - 1:
+                    diceStatement += "\n"
+        return "\nI am %s. %s" % (self.name, diceStatement)
+
+    def getDie(self, die):
+        self.dice.append(die)
+        print("\n%s now owns a new die with %s sides." % (self.name, str(die.numSides)))
+        die.owner = self
 
 class Die(object):
     """represents a single die, takes a name and an array of sides"""
 
-    def __init__(self, name, sides):
-        self.name = str(name)
+    def __init__(self, sides, owner = None):
         self.sides = sides
         self.numSides = len(sides)
+        if owner == None:
+            self.owner = None
+            self.name = "Unnamed Die with %s Sides" % (str(self.numSides))
+        else:
+            self.owner = owner
+            self.name = "%s's Die with %s Sides" % (owner.name.capitalize(), str(self.numSides))
+            owner.getDie(self)
 
     def __str__(self):
-        sideStringList = ""
-        before = ""
-        for side in range(len(self.sides)):
-            if side == 0:
-                before = ""
-            elif side == len(self.sides) - 1:
-                before = " and "
-            else:
-                before = ", "
-            sideStringList += before + str(self.sides[side])
-        return "I am %s and I have a %s-sided die with values %s." % (self.name, self.numSides, sideStringList)
+        return "Sides: %s | Values %s | Owner: %s" % (str(self.numSides), buildListString(self.sides), self.owner.name)
+
+    def assignOwner(self, owner):
+        self.owner = owner
+        print("\n%s now owns a new die with %s sides." % (owner.name, str(self.numSides)))
+        owner.getDie(self)
 
     def roll(self):
         rolledSide = random.randint(0, len(self.sides) - 1)
-        theRoll = Roll(self.name, self.sides[rolledSide])
-        return theRoll
+        return Roll(self, self.sides[rolledSide])
 
 class Roll(object):
     """represents the results of a die roll, takes a name and an array of sides"""
@@ -45,14 +74,20 @@ class Roll(object):
         self.result = result
 
     def __str__(self):
-        return "The roll result of the %s's die is %s." % (self.die, str(self.result))
+        ownerTag = self.die.owner.name + ": " if self.die.owner != None else ""
+        return "\n%sThe roll result is %s." % (ownerTag, str(self.result))
 
     def compareTo(self, anotherRoll):
+        ownerTag = self.die.owner.name + ": " if self.die.owner != None else ""
+        printResult = "\n" + ownerTag
         if self.result > anotherRoll.result:
+            print(printResult + "I win! :)")
             return Result.win
         elif self.result < anotherRoll.result:
+            print(printResult + "I lose. :(")
             return Result.loss
         elif self.result == anotherRoll.result:
+            print(printResult + "We tied.")
             return Result.tie
 
 class RollGroup(object):
@@ -87,15 +122,15 @@ class RollGroup(object):
         # Add the winner object properties
         self.winners = winnerList
         self.winningResult = self.winners[0].result
-        self.winningDies = buildListSting(self.winners)
+        self.winningDies = buildListString(self.winners)
         # Add the runner up object properties
         self.runnersUp = runnersUpList
         self.runnersUpResult = self.runnersUp[0].result
-        self.runnersUpDies = buildListSting(self.runnersUp)
+        self.runnersUpDies = buildListString(self.runnersUp)
         # Add the last place object properties
         self.lastPlace = lastPlaceList
         self.lastPlaceResult = self.lastPlace[0].result
-        self.lastPlaceDies = buildListSting(self.lastPlace)
+        self.lastPlaceDies = buildListString(self.lastPlace)
 
     def __str__(self):
         return "There were %s rolls. %s was the winner with a roll of %s beating %s with a roll of %s. %s was last with a roll of %s." % (self.rollCount, self.winningDies, self.winningResult, self.runnersUpDies, self.runnersUpResult, self.lastPlaceDies, self.lastPlaceResult)
@@ -105,18 +140,26 @@ class Result(enum.Enum):
     tie = 0
     loss = -1
 
+    def __str__(self):
+        if self == Result.win:
+            return "\nWin"
+        elif self == Result.loss:
+            return "\nLoss"
+        else:
+            return "\nTie"
+
 # Define Functions to Run Program
 #_________________________________________________________________________________________
 
 # Build a string describing a list
-def buildListSting(inputList):
+def buildListString(inputList):
     outputString = ""
     for i in range(len(inputList)):
         if i == len(inputList) - 1 and i != 0:
-            outputString.append("and ")
+            outputString += " and "
         elif i != len(inputList) - 1 and i != 0:
-            outputString.append(", ")
-        outputString.append(inputList[i])
+            outputString += ", "
+        outputString += str(inputList[i])
     return outputString
 
 # Returns a roll object given a die
@@ -165,20 +208,31 @@ def rollXTime(timesToRoll, firstDie, secondDie):
 #_________________________________________________________________________________________
 
 #Load the CSV file to a variable
-timesToRoll = int(input("How many times would you like to roll? "))
+# timesToRoll = int(input("How many times would you like to roll? "))``
+timesToRoll = 100
 
-#Makes the dice
-Katherine = Die("Katherine",[3,3,3,3,3,6])
-Zack = Die("Zack",[2,2,2,5,5,5])
+# Makes the people
+Katherine = Person("Katherine")
+Zack = Person("Zack")
+
+# Makes the dice
+die01 = Die([3,3,3,3,3,6], Katherine)
+die02 = Die([2,2,2,5,5,5], Zack)
 print(Katherine)
 print(Zack)
 
+roll01 = die01.roll()
+roll02 = die02.roll()
+print(roll01)
+print(roll02)
+print(roll01.compareTo(roll02))
+
 #Perform the roll
-results = rollXTime(timesToRoll, Katherine, Zack)
-print("%s: %s, %s: %s, Tied: %s" % (Katherine.name, results[0], Zack.name, results[1], results[2]))
-if results[0] > results[1] and results[0] > results[2]:
-    print("%s won with %d%%." % (Katherine.name, 100*(float(results[0])/float(timesToRoll))))
-if results[1] > results[0] and results[1] > results[2]:
-    print("%s won with %d%%." % (Zack.name, 100*(float(results[1])/float(timesToRoll))))
-if results[2] > results[0] and results[2] > results[1]:
-    print("The ties won with %d%%." % 100*(float(results[2])/float(timesToRoll)))
+# results = rollXTime(timesToRoll, Katherine, Zack)
+# print("%s: %s, %s: %s, Tied: %s" % (Katherine.name, results[0], Zack.name, results[1], results[2]))
+# if results[0] > results[1] and results[0] > results[2]:
+#     print("%s won with %d%%." % (Katherine.name, 100*(float(results[0])/float(timesToRoll))))
+# if results[1] > results[0] and results[1] > results[2]:
+#     print("%s won with %d%%." % (Zack.name, 100*(float(results[1])/float(timesToRoll))))
+# if results[2] > results[0] and results[2] > results[1]:
+#     print("The ties won with %d%%." % 100*(float(results[2])/float(timesToRoll)))
